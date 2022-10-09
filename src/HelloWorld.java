491bf70c21777a2826f09e7d0bf39bdd34391fe0
HR <hos.raz1993@gmail.com>
2022-10-09 18:07:15 +0300

    Edited

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello world!");
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
